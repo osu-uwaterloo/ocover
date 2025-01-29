f4761f7a70627f65626f49a0c5faf53b5ebaa2af
solstice23 <solstice233@gmail.com>
2025-01-29 13:31:07 -0500

    first commit

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 import express from 'express';
-import NodeCache from 'node-cache';
-import * as api from './api.js';
 
-const cacheControl = new NodeCache({ stdTTL: 86400, checkperiod: 600, deleteOnExpire: false });
 const app = express();
 
-app.get('/:name', async function (req, res) {
+app.get('/:seg', async function (req, res) {
 	res.set({
-		'Content-Type': 'image/png',
-		'Cache-Control': 'public, max-age=86400',
+		'Content-Type': 'image/svg+xml',
 	});
-	let username = req.params.name ?? '';
-
-	let userData, coverUrl;
-	let cacheKey = username;
-
-	if (req.headers['cache-control'] != 'no-cache' && cacheControl.has(cacheKey)) {
-		({ coverUrl } = cacheControl.get(cacheKey));
-		if (cacheControl.ttl(cacheKey) < 3600) {
-			api.getUser(username).then((data) => {
-				if (!data.error) {
-					cacheControl.set(cacheKey, { coverUrl: data.user.cover_url });
+	const seg = req.params.seg.padEnd(7, '0');
+	const bits = seg.split('').map(c => c === '1' ? 1 : 0);
+	res.send(`
+		<?xml version="1.0" encoding="UTF-8"?>
+		<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 207.21 379.97">
+			<style>
+				.active {
+				fill: #f00;
 				}
-			});
-		}
-	} else {
-		userData = await api.getUser(username);
-		if (userData.error) {
-			res.set('Content-Type', 'text/plain');
-			res.send('Error: ' + userData.error);
-			return;
-		}
-		coverUrl = userData.user.cover_url;
-		cacheControl.set(cacheKey, { coverUrl });
-	}
-	res.redirect(302, coverUrl);
+			</style>
+			<polygon class="${bits[6] ? 'active' : ''}" data-name="7" points="181.24 362.48 164.02 379.97 43.19 379.97 25.97 362.48 43.19 345.26 164.02 345.26 181.24 362.48" stroke-width="0"/>
+			<polygon class="${bits[5] ? 'active' : ''}" data-name="6" points="207.21 215.95 207.21 336.78 189.99 354 172.76 336.78 172.76 215.95 189.99 198.73 207.21 215.95" stroke-width="0"/>
+			<polygon class="${bits[4] ? 'active' : ''}" data-name="5" points="34.45 215.95 34.45 336.78 17.22 354 0 336.78 0 215.95 17.22 198.73 34.45 215.95" stroke-width="0"/>
+			<polygon class="${bits[3] ? 'active' : ''}" data-name="4" points="181.24 189.85 164.02 207.34 43.19 207.34 25.97 189.85 43.19 172.63 164.02 172.63 181.24 189.85" stroke-width="0"/>
+			<polygon class="${bits[2] ? 'active' : ''}" data-name="3" points="207.21 43.19 207.21 164.02 189.99 181.24 172.76 164.02 172.76 43.19 189.99 25.97 207.21 43.19" stroke-width="0"/>
+			<polygon class="${bits[1] ? 'active' : ''}" data-name="2" points="34.45 43.19 34.45 164.02 17.22 181.24 0 164.02 0 43.19 17.22 25.97 34.45 43.19" stroke-width="0"/>
+			<polygon class="${bits[0] ? 'active' : ''}" data-name="1" points="181.24 17.22 164.02 34.71 43.19 34.71 25.97 17.22 43.19 0 164.02 0 181.24 17.22" stroke-width="0"/>
+		</svg>
+	`.trim());
 });
 
 app.listen(process.env.PORT || 3000);
